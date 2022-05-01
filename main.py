5b011e8b373de19f7032d6a7539950c1c2a299b0
raxiou <raxiou80@gmail;com>
2022-05-01 11:08:18 +0200

    affichage terrain

--- main.py
+++ main.py
@@ -3,21 +3,33 @@ from objet.personnage import Personage
 from time import sleep
 import keyboard
 
+def affichage_terrain(terrain):
+    for liste in terrain.affiche:
+        print(liste)
+    print('--------------')
+
+
 def jeu():
     joueur = Personage('philipe', 'joueur', (0, 0))
     terrain = Terrain(joueur)
     arret = False
+    affichage_terrain(terrain)
+    print(terrain.affiche[0][6])
+    print(terrain.affiche[6][0])
     while not arret:
-        print(terrain.affiche)
         lettre = keyboard.read_key()
         if lettre == 'd':
             joueur.deplacement_droit(terrain)
+            affichage_terrain(terrain)
         elif lettre == 'q':
             joueur.deplacement_gauche(terrain)
+            affichage_terrain(terrain)
         elif lettre == 'z':
             joueur.deplacement_haut(terrain)
+            affichage_terrain(terrain)
         elif lettre == 's':
             joueur.deplacement_bas(terrain)
+            affichage_terrain(terrain)
         sleep(1)
 
 
